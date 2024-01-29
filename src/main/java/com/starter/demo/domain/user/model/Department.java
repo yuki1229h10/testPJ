95cc936f5cf83edaa0ce266b9d13e2027e845161
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-01-29 14:51:51 +0900

    combine department with user

--- src/main/java/com/starter/demo/domain/user/model/Department.java
+++ src/main/java/com/starter/demo/domain/user/model/Department.java
@@ -2,6 +2,11 @@ package com.starter.demo.domain.user.model;
 
 import lombok.Data;
 
+/**
+ * 部署クラスのエンティティクラスです。
+ * @author Yuki
+ *
+ */
 @Data
 public class Department {
 	private Integer departmentId;
--- src/main/java/com/starter/demo/form/UserDetailForm.java
+++ src/main/java/com/starter/demo/form/UserDetailForm.java
@@ -2,8 +2,15 @@ package com.starter.demo.form;
 
 import java.util.Date;
 
+import com.starter.demo.domain.user.model.Department;
+
 import lombok.Data;
 
+/**
+ * ユーザフォーム
+ * @author Yuki
+ *
+ */
 @Data
 public class UserDetailForm {
 	private String userId;
@@ -12,4 +19,7 @@ public class UserDetailForm {
 	private Date birthday;
 	private Integer age;
 	private Integer gender;
+
+	// 部署
+	private Department department;
 }
